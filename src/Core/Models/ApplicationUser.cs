88d4d0a062ca9dd10774d22e5e565d91154a2af0
mohmedeprahem <m.eprahem.168@gmail.com>
2023-11-29 15:56:37 +0200

    feat: create ApplicationUser entity in core/Models folder.
    properties of this class added: FirstName, LastName, Email, Gender, DateOfBirth and Image

---
+++ src/Core/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Core.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string Gender;
+        public DateTime DateOfBirth;
+        public string? Image;
+    }
+}
