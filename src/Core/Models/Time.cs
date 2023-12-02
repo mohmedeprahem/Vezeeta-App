f0bd301a292b895e1db29f4b957b0ddbb15ab71f
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-02 19:25:23 +0200

    feat: Add Time entity validation

--- src/Core/Models/Time.cs
+++ src/Core/Models/Time.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace Core.Models
     public class Time
     {
         public int Id { get; set; }
-        public string TimeValue { get; set; }
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:h:mm tt}")]
+        public DateTime TimeValue { get; set; }
         public ICollection<AppointmentTime> Appointments { get; set; }
     }
 }
