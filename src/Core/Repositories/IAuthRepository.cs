e8f69d5f1e4f21dd0c48099b255ea70eaf1aca8e
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-04 16:56:27 +0200

    feat: Create AuthRepository
    - Register Mether

---
+++ src/Core/Repositories/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Core.Repositories
+{
+    public interface IAuthRepository
+    {
+        public Task<IdentityResult> Register(ApplicationUser user, string password);
+    }
+}
---
+++ src/Infrastructure/Repositories/AuthRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+using Core.Repositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthRepository(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> Register(ApplicationUser user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+    }
+}
