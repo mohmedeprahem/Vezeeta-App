aa165f38e7a651aac18596e5c9204503ac660529
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-05 00:44:49 +0200

    feat: Create file Service ( UploadFile method  )

---
+++ src/Application/Interfaces/Services/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Interfaces.Services
+{
+    public interface IFileService
+    {
+        public Task<string> UploadFile(IFormFile file, string SavePath);
+    }
+}
---
+++ src/Infrastructure/Helpers/FileService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Helpers
+{
+    public class FileService : IFileService
+    {
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public FileService(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        public async Task<string> UploadFile(IFormFile file, string SavePath)
+        {
+            // Use a unique identifier (e.g., a GUID)
+            string uniqueId = Guid.NewGuid().ToString();
+
+            // Use a timestamp to ensure uniqueness
+            string timestamp = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+
+            // Combine the unique identifier and timestamp
+            string imageName = $"{uniqueId}_{timestamp}{Path.GetExtension(file.FileName)}";
+
+            string filePath = Path.Combine(SavePath, imageName);
+
+            using var stream = File.Create(filePath);
+
+            await file.CopyToAsync(stream);
+
+            return filePath;
+        }
+    }
+}
