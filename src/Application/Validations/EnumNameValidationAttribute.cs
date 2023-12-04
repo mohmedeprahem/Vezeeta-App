3564192a129ba277886d4107e033ae675585b37e
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-04 16:14:49 +0200

    feat: Create costum validation attribute (EnumNameValidationAttribute.cs)

---
+++ src/Application/Validations/EnumNameValidationAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validations
+{
+    [AttributeUsage(
+        AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false
+    )]
+    public class EnumNameValidationAttribute : ValidationAttribute
+    {
+        private readonly Type _enumType;
+
+        public EnumNameValidationAttribute(Type enumType) => _enumType = enumType;
+
+        protected override ValidationResult IsValid(
+            object value,
+            ValidationContext validationContext
+        )
+        {
+            if (value != null)
+            {
+                string enumName = value.ToString();
+
+                if (Enum.IsDefined(_enumType, enumName))
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult(
+                        $"The {validationContext.DisplayName} field must have a valid enum name."
+                    );
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
