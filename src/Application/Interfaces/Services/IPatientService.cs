711336e616647221f65d7bbd5ab07a7e9bf1b02c
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-05 22:34:00 +0200

    feat: Create PatientService
    - Add Methods (GetPatients, GetPatientsCount)

---
+++ src/Application/Interfaces/Services/IPatientService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Application.Interfaces.Services
+{
+    public interface IPatientService
+    {
+        public Task<List<ApplicationUser>> GetPatients(int page, int size);
+        public Task<int> GetPatientsCount();
+    }
+}
---
+++ src/Infrastructure/Services/PatientService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using Azure;
+using Core.enums;
+using Core.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Services
+{
+    public class PatientService : IPatientService
+    {
+        private readonly IPatientRepository _patientRepository;
+
+        public PatientService(IPatientRepository patientRepository)
+        {
+            this._patientRepository = patientRepository;
+        }
+
+        public async Task<List<ApplicationUser>> GetPatients(int page, int size)
+        {
+            return await _patientRepository.GetUsersByRole(
+                RolesEnum.Patient.ToString(),
+                page,
+                size
+            );
+        }
+
+        public async Task<int> GetPatientsCount()
+        {
+            return await _patientRepository.GetUsersCountByRole(RolesEnum.Patient.ToString());
+        }
+    }
+}
