a3a12c518d21c8ded1472c755462375ab434a0a8
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-07 01:29:31 +0200

    feat: Create DoctorRepository
    - Adding GetDoctors method

---
+++ src/Application/Interfaces/Repositories/IDoctorRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IDoctorRepository : IUserRepository
+    {
+        public Task<List<ApplicationUser>> GetDoctors(
+            int page,
+            int size,
+            string search = "",
+            string[] includes = null
+        );
+    }
+}
---
+++ src/Infrastructure/Repositories/DoctorRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces.Repositories;
+using Core.Models;
+using Infrastructure.DataBase.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class DoctorRepository : UserRepository, IDoctorRepository
+    {
+        public DoctorRepository(UserManager<ApplicationUser> userManager, AppDbContext appDbContext)
+            : base(userManager, appDbContext) { }
+
+        public async Task<List<ApplicationUser>> GetDoctors(
+            int page = 1,
+            int size = 8,
+            string search = "",
+            string[] includes = null
+        )
+        {
+            IQueryable<ApplicationUser> query = _appDbContext.Set<ApplicationUser>();
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            List<ApplicationUser> pagedUsers = await query
+                .Where(
+                    user =>
+                        user.FullName.Contains(search)
+                        || user.Email.Contains(search)
+                        || user.PhoneNumber.Contains(search)
+                )
+                .Skip((page - 1) * size)
+                .Take(size)
+                .ToListAsync();
+            return pagedUsers;
+        }
+    }
+}
