3d0fe2577102234e6d669e2687f5c952122c23d8
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-06 21:12:36 +0200

    feat: Create doctorDto

---
+++ src/Application/Dtos/DoctorDto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Validations;
+using Core.enums;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Dtos
+{
+    public class DoctorDto
+    {
+        [StringLength(50, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(50, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [EnumNameValidation(typeof(GendersEnum))]
+        public string Gender { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string Specialize { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
