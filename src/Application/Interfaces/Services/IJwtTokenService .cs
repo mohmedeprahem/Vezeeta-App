37c4e757aaff8202a66e8daff79076fedf186724
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-06 21:21:04 +0200

    fix: Remove line in IJwtTokenService
    - remove using Core.Authentications;

--- src/Application/Interfaces/Services/IJwtTokenService .cs
+++ src/Application/Interfaces/Services/IJwtTokenService .cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Core.Authentications;
 using Core.Models;
 
 namespace Application.Interfaces.Services
