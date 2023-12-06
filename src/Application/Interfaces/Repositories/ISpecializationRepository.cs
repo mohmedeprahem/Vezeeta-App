fb55618272e4f9c7428f069c6a6b42366a74f2d1
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-06 21:15:21 +0200

    feat: Create specializationRepository
    - add GetSpecializationByTitle method

---
+++ src/Application/Interfaces/Repositories/ISpecializationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface ISpecializationRepository
+    {
+        public Task<Specialization> GetSpecializationByTitle(string title);
+    }
+}
---
+++ src/Infrastructure/Repositories/SpecializationRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces.Repositories;
+using Core.Models;
+using Infrastructure.DataBase.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class SpecializationRepository : ISpecializationRepository
+    {
+        public readonly AppDbContext _appDbContext;
+
+        public SpecializationRepository(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        public async Task<Specialization> GetSpecializationByTitle(string title)
+        {
+            return await _appDbContext.Specializations.FirstOrDefaultAsync(s => s.Title == title);
+        }
+    }
+}
