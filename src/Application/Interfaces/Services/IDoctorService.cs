ad89c20933f76551055aa6c7a81fe98d9f044fcb
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-07 01:30:03 +0200

    feat: Create DoctorService
    - Adding GetDoctors method

--- src/Application/Interfaces/Services/IDoctorService.cs
+++ src/Application/Interfaces/Services/IDoctorService.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Models;
 
 namespace Application.Interfaces.Services
 {
-    public interface IDoctorService { }
+    public interface IDoctorService
+    {
+        public Task<List<ApplicationUser>> GetDoctors(int page, int size, string search);
+        public Task<int> GetDoctorsCount(string lastDate = "");
+    }
 }
--- src/Infrastructure/Services/DoctorService.cs
+++ src/Infrastructure/Services/DoctorService.cs
@@ -4,13 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Dtos;
+using Application.Interfaces.Repositories;
 using Application.Interfaces.Services;
+using Core.enums;
+using Core.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Services
 {
     public class DoctorService : IDoctorService
     {
-        public DoctorService() { }
+        private readonly IDoctorRepository _doctorRepository;
+
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            this._doctorRepository = doctorRepository;
+        }
+
+        public async Task<List<ApplicationUser>> GetDoctors(int page, int size, string search)
+        {
+            return await _doctorRepository.GetDoctors(
+                page = 1,
+                size = 8,
+                search = "",
+                new string[] { "Specialization" }
+            );
+        }
+
+        public async Task<int> GetDoctorsCount(string lastDate = "")
+        {
+            return await _doctorRepository.GetUsersCountByRole(
+                RolesEnum.Doctor.ToString(),
+                lastDate
+            );
+        }
+
+        public async Task<ApplicationUser> GetPatientById(string id)
+        {
+            return await _doctorRepository.GetUserById(id);
+        }
     }
 }
