6c453862a041e98ef63cf1b730759bf94ac7410c
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-06 23:25:19 +0200

    feat: Create DoctorService

---
+++ src/Infrastructure/Services/DoctorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Dtos;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Services
+{
+    public class DoctorService : IDoctorService
+    {
+        public DoctorService() { }
+    }
+}
