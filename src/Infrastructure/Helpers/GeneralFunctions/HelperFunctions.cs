a27b0fa9def7c920ad4b2c9f2459e46c6db8491d
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-08 22:33:57 +0200

    feat: Create HelperFunction Class
    - Adding method GetNextWeekday

---
+++ src/Infrastructure/Helpers/GeneralFunctions/HelperFunctions.cs
@@ -0,0 +1,28 @@
+﻿using Core.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Helpers.GeneralFunctions
+{
+    public class HelperFunctions
+    {
+        public DateTime GetNextWeekday(string day)
+        {
+            DateTime currentDate = DateTime.UtcNow;
+            DayOfWeek currentDayOfWeek = currentDate.DayOfWeek;
+
+            DayOfWeek targetDay = Enum.Parse<DayOfWeek>(day);
+
+            // Calculate the number of days until the target day in the next week
+            int daysUntilTargetDay = ((int)targetDay - (int)currentDayOfWeek + 7) % 7;
+
+            // Add the days to the current date to get the target day in the next week
+            DateTime nextWeekday = currentDate.AddDays(daysUntilTargetDay);
+
+            return nextWeekday;
+        }
+    }
+}
