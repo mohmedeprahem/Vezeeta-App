da073d764b0ef7403e20c294795b15ab034fc1bd
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-08 22:32:50 +0200

    feat: Add new column in Appointment entity (Date)

--- src/Core/Models/Appointment.cs
+++ src/Core/Models/Appointment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,10 @@ namespace Core.Models
         public int Id { get; set; }
         public string DoctorId { get; set; }
         public int DayId { get; set; }
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
         public ICollection<AppointmentTime> Times { get; set; }
         public ApplicationUser Doctor { get; set; }
         public Day Day { get; set; }
