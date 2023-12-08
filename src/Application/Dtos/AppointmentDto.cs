5220ef7e702c4a44c041ec4a04d79a5e7299ea80
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-08 22:34:49 +0200

    feat: Create CreateAppointmetDto

---
+++ src/Application/Dtos/AppointmentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Validations;
+using Core.enums;
+
+namespace Application.Dtos
+{
+    public class AppointmentDto
+    {
+        [Required]
+        [EnumNameValidation(typeof(DaysEnum))]
+        public string Day { get; set; }
+
+        [Required]
+        public List<string> Times { get; set; }
+    }
+}
---
+++ src/Application/Dtos/CreateAppointmentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class CreateAppointmentDto
+    {
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        public List<AppointmentDto> Appointments { get; set; }
+    }
+}
