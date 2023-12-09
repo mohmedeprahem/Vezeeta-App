09427e470efd1e5186bb9be5af97f6f8baa0e03f
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-09 06:11:06 +0200

    feat: Create CreateDiscountDto

---
+++ src/Application/Dtos/CreateDiscountDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class CreateDiscountDto
+    {
+        [MinLength(6)]
+        [MaxLength(6)]
+        public string DiscountCode { get; set; }
+        public int DiscountTypeId { get; set; }
+        public bool IsActivated { get; set; }
+        public int DiscountValue { get; set; }
+    }
+}
