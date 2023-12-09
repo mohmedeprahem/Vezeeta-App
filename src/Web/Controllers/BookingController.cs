ad5d831a3d704d9405b19ff78e7a4b66e49176c9
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-09 23:01:45 +0200

    feat: Create CreateBooking api

---
+++ src/Web/Controllers/BookingController.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Claims;
+using Application.Dtos;
+using Application.Interfaces.Services;
+using Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/booking")]
+    [ApiController]
+    public class BookingController : Controller
+    {
+        private readonly IBookingService _bookingService;
+
+        public BookingController(IBookingService bookingService)
+        {
+            this._bookingService = bookingService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking([FromBody] CreateBookingDto createBookingDto)
+        {
+            try
+            {
+                // Access claims from the current user's ClaimsPrincipal
+                ClaimsPrincipal user = HttpContext.User;
+
+                // Get the value of a specific claim
+                string? userId = user.FindFirst("Id")?.Value;
+                if (userId == null)
+                {
+                    return Unauthorized();
+                }
+
+                IdentityResult bookingResult = await _bookingService.CreateBookingAsync(
+                    createBookingDto.AppointmentTimeId,
+                    createBookingDto.DiscountCode,
+                    userId
+                );
+                if (!bookingResult.Succeeded)
+                {
+                    if (bookingResult.Errors.Any(error => error.Code == "DiscountCode"))
+                        return BadRequest(bookingResult);
+                    return Unauthorized();
+                }
+
+                return StatusCode(
+                    201,
+                    new
+                    {
+                        succes = true,
+                        statusCode = 201,
+                        message = "Booking created successfully"
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+            }
+        }
+    }
+}
