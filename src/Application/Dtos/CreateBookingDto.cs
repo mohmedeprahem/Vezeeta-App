53f1543d6730db9312785bf1b41ab8c1cdbb1687
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-09 22:50:21 +0200

    feat: create CreateBookingDto

---
+++ src/Application/Dtos/CreateBookingDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class CreateBookingDto
+    {
+        public int AppointmentTimeId { get; set; }
+
+        [MinLength(6)]
+        [MaxLength(6)]
+        public string? DiscountCode { get; set; }
+    }
+}
