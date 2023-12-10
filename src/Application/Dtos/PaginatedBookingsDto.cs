027b4066641e6645250ed1debf7fdae6e71bd295
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-10 16:15:37 +0200

    feat: Create PaginatedBookingsDto class

---
+++ src/Application/Dtos/PaginatedBookingsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Application.Dtos
+{
+    public class PaginatedBookingsDto
+    {
+        public int TotalBookings { get; set; }
+        public int MaxPages { get; set; }
+        public List<Booking> Bookings { get; set; }
+    }
+}
