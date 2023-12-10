a199d247103215e1eb799c6be4433f8feea0ec3d
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-10 09:26:19 +0200

    feat: Create BookingDto and PatientAllInfoDto

---
+++ src/Application/Dtos/BookingDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class BookingDto
+    {
+        public string Image { get; set; }
+        public string DoctorName { get; set; }
+        public string Specialize { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+        public decimal Price { get; set; }
+        public string DiscountCode { get; set; }
+        public decimal FinalPrice { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ src/Application/Dtos/PatientAllInfoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class PatientAllInfoDto
+    {
+        public string Image { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+        public List<BookingDto> Requests { get; set; }
+    }
+}
