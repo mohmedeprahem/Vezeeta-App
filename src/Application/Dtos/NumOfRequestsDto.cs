65debef81e09f3cec1d4fe1dba9cff6e3af8e7c7
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-10 07:31:29 +0200

    feat: Create NumOfRequestsDto class

---
+++ src/Application/Dtos/NumOfRequestsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class NumOfRequestsDto
+    {
+        public int ConfirmedRequests { get; set; }
+        public int PendingRequests { get; set; }
+        public int CancelledRequests5 { get; set; }
+    }
+}
