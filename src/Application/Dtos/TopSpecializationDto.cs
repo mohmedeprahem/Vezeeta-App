ec24b06a79da10fc099f2a0666f3e37784507cb9
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-10 08:16:01 +0200

    feat: Create TopSpecializationDto class

---
+++ src/Application/Dtos/TopSpecializationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class TopSpecializationDto
+    {
+        public string FullName { get; set; }
+        public int BookingCount { get; set; }
+    }
+}
