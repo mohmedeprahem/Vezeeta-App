3d2249c8b8a32c1822c272d7025b3b3fbb4e077b
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-10 03:48:17 +0200

    fix: Add relationship one to many between AppointmentTime and Booking

---
+++ src/Infrastructure/Migrations/20231210014359_oneToMany_Appointmenttime_Booking.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class oneToMany_Appointmenttime_Booking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_AppointmentTimeId",
+                table: "Bookings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_AppointmentTimeId",
+                table: "Bookings",
+                column: "AppointmentTimeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_AppointmentTimeId",
+                table: "Bookings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_AppointmentTimeId",
+                table: "Bookings",
+                column: "AppointmentTimeId",
+                unique: true);
+        }
+    }
+}
