990e7336aefbf506ae0e5e877981b7f8a29c3411
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-10 05:05:13 +0200

    feat: Create UpdateDiscountDto class

---
+++ src/Application/Dtos/UpdateDiscountDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class UpdateDiscountDto
+    {
+        [MinLength(6)]
+        [MaxLength(6)]
+        public string DiscountCode { get; set; }
+        public int DiscountTypeId { get; set; }
+        public int DiscountValue { get; set; }
+    }
+}
