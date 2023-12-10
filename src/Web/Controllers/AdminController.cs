604542754611656bd9dfb8c769a6160fcac155c9
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-10 06:12:40 +0200

    refactor: Move some methods from admin controller to doctor controller
    - GetDoctorsCount
    - UpdateDoctor

--- src/Web/Controllers/AdminController.cs
+++ src/Web/Controllers/AdminController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
-    [Route("api/admin/doctors")]
+    [Route("api/admin")]
     [ApiController]
     public class AdminController : Controller
     {
@@ -20,7 +20,7 @@ namespace Web.Controllers
             this._doctorService = doctorService;
         }
 
-        [HttpGet]
+        [HttpGet("doctors")]
         [Authorize(policy: "AdminOnly")]
         public async Task<IActionResult> GetDoctors(
             [FromQuery] int page = 1,
@@ -90,29 +90,7 @@ namespace Web.Controllers
             }
         }
 
-        [HttpGet("count")]
-        [Authorize(policy: "AdminOnly")]
-        public async Task<IActionResult> GetDoctorsCount([FromQuery] string lastDate = "")
-        {
-            try
-            {
-                int numberOfDoctors = await _doctorService.GetDoctorsCount(lastDate);
-                return Ok(
-                    new
-                    {
-                        numberOfDoctors,
-                        succes = true,
-                        statusCode = 200
-                    }
-                );
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, "Internal Server Error");
-            }
-        }
-
-        [HttpGet("{Id}")]
+        [HttpGet("doctors/{Id}")]
         [Authorize(policy: "AdminOnly")]
         public async Task<IActionResult> GetDoctorById([FromRoute] string Id)
         {
@@ -151,67 +129,7 @@ namespace Web.Controllers
             }
         }
 
-        [HttpPut("{doctorId}")]
-        [Authorize(policy: "AdminOnly")]
-        public async Task<IActionResult> UpdateDoctor(
-            [FromRoute] string doctorId,
-            [FromForm] UpdateDoctorDto doctorDto
-        )
-        {
-            try
-            {
-                // Validate image
-                if (doctorDto.Image != null)
-                {
-                    long fileSize = doctorDto.Image.Length;
-
-                    if (
-                        doctorDto.Image.ContentType != "image/jpeg"
-                        && doctorDto.Image.ContentType != "image/png"
-                    )
-                    {
-                        ModelState.AddModelError(
-                            "Image",
-                            "Invalid image format. Only JPEG and PNG are allowed."
-                        );
-                    }
-                    else if (fileSize > 5 * 1024 * 1024)
-                    {
-                        ModelState.AddModelError(
-                            "Image",
-                            "Invalid image size. The maximum allowed size is 5 MB."
-                        );
-                    }
-                }
-
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-
-                IdentityResult statusDoctorUpdate = await _doctorService.UpdateDoctor(
-                    doctorId,
-                    doctorDto
-                );
-
-                if (!statusDoctorUpdate.Succeeded)
-                {
-                    return BadRequest(statusDoctorUpdate.Errors);
-                }
-
-                return Ok(
-                    new
-                    {
-                        succes = true,
-                        statusCode = 200,
-                        message = "Doctor updated successfully"
-                    }
-                );
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, "Internal Server Error");
-            }
+        
         }
     }
 }
--- src/Web/Controllers/DoctorController.cs
+++ src/Web/Controllers/DoctorController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Dtos;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
@@ -6,9 +10,95 @@ namespace Web.Controllers
     [ApiController]
     public class DoctorController : Controller
     {
-        public IActionResult Index()
+        private readonly IDoctorService _doctorService;
+
+        public DoctorController(IDoctorService doctorService)
         {
-            return View();
+            this._doctorService = doctorService;
         }
-    }
+
+        [HttpGet("count")]
+        [Authorize(policy: "AdminOnly")]
+        public async Task<IActionResult> GetDoctorsCount([FromQuery] string lastDate = "")
+        {
+            try
+            {
+                int numberOfDoctors = await _doctorService.GetDoctorsCount(lastDate);
+                return Ok(
+                    new
+                    {
+                        numberOfDoctors,
+                        succes = true,
+                        statusCode = 200
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpPut("{doctorId}")]
+        [Authorize(policy: "AdminOnly")]
+        public async Task<IActionResult> UpdateDoctor(
+            [FromRoute] string doctorId,
+            [FromForm] UpdateDoctorDto doctorDto
+        )
+        {
+            try
+            {
+                // Validate image
+                if (doctorDto.Image != null)
+                {
+                    long fileSize = doctorDto.Image.Length;
+
+                    if (
+                        doctorDto.Image.ContentType != "image/jpeg"
+                        && doctorDto.Image.ContentType != "image/png"
+                    )
+                    {
+                        ModelState.AddModelError(
+                            "Image",
+                            "Invalid image format. Only JPEG and PNG are allowed."
+                        );
+                    }
+                    else if (fileSize > 5 * 1024 * 1024)
+                    {
+                        ModelState.AddModelError(
+                            "Image",
+                            "Invalid image size. The maximum allowed size is 5 MB."
+                        );
+                    }
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                IdentityResult statusDoctorUpdate = await _doctorService.UpdateDoctor(
+                    doctorId,
+                    doctorDto
+                );
+
+                if (!statusDoctorUpdate.Succeeded)
+                {
+                    return BadRequest(statusDoctorUpdate.Errors);
+                }
+
+                return Ok(
+                    new
+                    {
+                        succes = true,
+                        statusCode = 200,
+                        message = "Doctor updated successfully"
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
 }
